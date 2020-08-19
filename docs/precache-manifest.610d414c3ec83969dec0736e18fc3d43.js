55f80e9e064478e1193e712426b25d7913994a98
RicardoCGA <ricardo.cesar@live.com>
2020-08-19 17:39:21 -0500

    Docs Added

---
+++ docs/precache-manifest.610d414c3ec83969dec0736e18fc3d43.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cde4fce79aacd748fc9b6e13e60def31",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e2d849f1b99091c31fe",
+    "url": "/static/css/main.21054506.chunk.css"
+  },
+  {
+    "revision": "d31c915c60d1eed796f3",
+    "url": "/static/js/2.f8a005cb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f8a005cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e2d849f1b99091c31fe",
+    "url": "/static/js/main.146a40be.chunk.js"
+  },
+  {
+    "revision": "ff764c0e3a955e490620",
+    "url": "/static/js/runtime-main.21de4ef8.js"
+  }
+]);
